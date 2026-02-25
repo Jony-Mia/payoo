0532a5e66ea27529113b705db66a27211d144e48
Jony-Mia <ajonymia321@gmail.com>
2026-02-25 21:07:13 +0600

    payment button

--- main.js
+++
@@ -1,6 +0,0 @@
-// let setPhone = localStorage.setItem('phone','01770522886')
-// let setPin =  localStorage.setItem('pin','1122');
-let getPhone = localStorage.getItem('phone')
-let getPin =   localStorage.getItem('pin');
-
-console.log(getPhone, getPin);
--- pages/login.js
+++ pages/login.js
@@ -1,25 +1,18 @@
-function validate(e){
-    let phone = document.getElementById('phone').value;
-    let pin = document.getElementById('pin').value;
+let phone = document.getElementById('number');
+let pin = document.getElementById('pin')
+// let submit = document.getElementById('submit')
+//  localStorage.setItem('phone','01770522886');
+ localStorage.setItem('pin',1122)
+let localPhone = localStorage.getItem('phone');
+let localPin = localStorage.getItem('pin');
+submit.onclick=(e)=>{
+    e.preventDefault()
 
-    if(!(phone=== '' && pin==='')){
-         if(phone==='01770522886' && pin ==='1122'){
-         console.log('correct');
-         window.open('http://127.0.0.1:5500','_self')
+ localStorage.setItem('phone',phone);
+ localStorage.setItem('pin',Number(pin))
+}
 
-     }
-     else{
-        
-          alert('Wrong Password, Try again')
-      }
-    }else{
-        console.log('empty');
-        
-    }
-
-
-//   let state=   phone === '01770522886' && pin ==='1122' ? 'Correct' :'Wrong Password, Try again';
-//     console.log(state);
-    
-
+console.log(localPhone, localPin);
+if ((localPhone ==='01770522886' && localPin==='1122')) {
+    // window.open('http://127.0.0.1:5500','_self')
 }
